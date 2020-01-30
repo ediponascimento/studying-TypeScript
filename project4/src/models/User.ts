2d20c4e2654445f73f30035b7dc02ec1f5f7b6bb
Édipo Nascimento <ediposnascimento@gmail.com>
2020-01-30 07:39:40 -0300

    added Sync class

--- project4/src/models/User.ts
+++ project4/src/models/User.ts
@@ -3,7 +3,7 @@ import { UserProps } from '../interfaces/UserProps';
 import { Eventing } from './Eventing';
 
 export class User {
-  
+
   constructor(private data: UserProps) { }
   public events: Eventing = new Eventing();
 
@@ -13,22 +13,5 @@ export class User {
 
   set(user: UserProps): void {
     Object.assign(this.data, user);
-  }
-
-  fetch(): void {
-    axios.get(`http://localhost:3000/users/${this.get('id')}`)
-    .then((response: AxiosResponse): void => {
-      this.set(response.data);
-    });
-  }
-
-  save(): void {
-    const id = this.get('id');
-
-    if(id) {
-      axios.put(`http://localhost:3000/users/${id}`, this.data);
-    } else {
-      axios.post(` http://localhost:3000/users/`, this.data);
-    }
   }
 }
