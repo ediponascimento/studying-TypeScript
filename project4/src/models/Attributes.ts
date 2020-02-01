9e6941147bc0dc326d9dfd235bff298266f11011
Édipo Nascimento <ediposnascimento@gmail.com>
2020-02-01 08:52:39 -0300

    added type to the Attribute class

--- project4/src/models/Attributes.ts
+++ project4/src/models/Attributes.ts
@@ -1,11 +1,21 @@
 export class Attributes<T> {
   constructor(private data: T) { }
 
-  get(propName: string): number | string {
-    return this.data[propName];
+  get<K extends keyof T>(key: K): T[K] {
+    return this.data[key];
   }
 
   set(user: T): void {
     Object.assign(this.data, user);
   }
 }
+
+const attr = new Attributes({
+  id: 5,
+  age: 25,
+  name: 'Édipo Nascimento'
+});
+
+const id = attr.get('id');
+const name = attr.get('name');
+const age = attr.get('age');
