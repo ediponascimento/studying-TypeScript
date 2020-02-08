c63590201dff01f030a796d8734a1cb5368795ad
Édipo Nascimento <ediposnascimento@gmail.com>
2020-02-08 09:54:38 -0300

    added bingEvents function to the UserForm

--- project4/dist/src.f10117fe.js
+++ project4/dist/src.f10117fe.js
@@ -131,13 +131,42 @@ function () {
     this.parent = parent;
   }
 
+  UserForm.prototype.eventsMap = function () {
+    return {
+      'click:button': this.onButtonClick
+    };
+  };
+
+  UserForm.prototype.onButtonClick = function () {
+    console.log('Hi there');
+  };
+
   UserForm.prototype.template = function () {
-    return "\n      <div>\n        <h1>User Form</h1>\n        <input />\n      </div>\n    ";
+    return "\n      <div>\n        <h1>User Form</h1>\n        <input />\n        <button>Click Me</button>\n      </div>\n    ";
+  };
+
+  UserForm.prototype.bindEvents = function (fragment) {
+    var eventsMap = this.eventsMap();
+
+    var _loop_1 = function _loop_1(eventKey) {
+      var _a = eventKey.split(':'),
+          eventName = _a[0],
+          selector = _a[1];
+
+      fragment.querySelectorAll(selector).forEach(function (element) {
+        element.addEventListener(eventName, eventsMap[eventKey]);
+      });
+    };
+
+    for (var eventKey in eventsMap) {
+      _loop_1(eventKey);
+    }
   };
 
   UserForm.prototype.render = function () {
     var templateElement = document.createElement('template');
     templateElement.innerHTML = this.template();
+    this.bindEvents(templateElement.content);
     this.parent.append(templateElement.content);
   };
 
@@ -184,7 +213,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "39995" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45027" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
