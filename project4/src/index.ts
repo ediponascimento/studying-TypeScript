4fbc9472c45af1cd2a019ab8bc52d84f60e67a56
Édipo Nascimento <ediposnascimento@gmail.com>
2020-02-08 10:25:44 -0300

    binding User

--- project4/src/index.ts
+++ project4/src/index.ts
@@ -1,8 +1,11 @@
 import { UserForm } from '../src/views/UserForm';
+import { User } from './models/User';
+
+const user = User.buildUser({ name: 'Édipo', age: 25 });
 
 const userForm = new UserForm(
-    document.getElementById('root')
+    document.getElementById('root'),
+    user
 );
 
-
-userForm.render();
+userForm.render();  
--- project4/src/views/UserForm.ts
+++ project4/src/views/UserForm.ts
@@ -1,21 +1,25 @@
+import { User } from '../models/User';
+
 export class UserForm {
 
-  constructor(public parent: Element) { }
+  constructor(public parent: Element, public model: User) { }
 
   eventsMap(): { [key: string]: () => void } {
     return {
-        'click:button': this.onButtonClick
+      'click:button': this.onButtonClick
     };
   }
 
   onButtonClick(): void {
-    console .log('Hi there');
+    console.log('Hi there');
   }
 
   template(): string {
     return `
       <div>
         <h1>User Form</h1>
+        <div>User name: ${this.model.get('name')}</div>
+        <div>User age: ${this.model.get('age')}</div>
         <input />
         <button>Click Me</button>
       </div>
